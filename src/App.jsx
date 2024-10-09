f35aaf12b32611c0ff3c8fa75e6f49d0b890f9e3
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-10-09 23:45:42 +0530

    Work part

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Skill from './components/Skill'
+import Work from './components/Work'
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Hero/>
       <About />
       <Skill />
+      <Work />
     </main>
     </>
   )
