0605e688720eb62c7deb45e8affde0237abcc60f
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-10-14 12:30:53 +0530

    Contact page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skill from './components/Skill'
 import Work from './components/Work'
+import Review from './components/Review'
+import Contact from './components/Contact'
 import './App.css'
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
       <About />
       <Skill />
       <Work />
+      <Review />
+      <Contact />
     </main>
     </>
   )
